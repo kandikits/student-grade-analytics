0489679bf72e137ec5bb3e993d353ea76767c0d9
kandikits <95465112+kandikits@users.noreply.github.com>
2022-07-26 14:39:32 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -13,10 +13,8 @@ OUTPUT_PRED_FILE="output.csv"
 OUTPUT_PRED="y_pred"
 MODEL_FILE="model"
 
-df_train=pd.read_csv(INPUT_TRAIN_FILE)
-df_test=pd.read_csv(INPUT_TEST_FILE)
-
-def train(target = 'G3', numeric_features=["age", "G1", "G2"]):
+def train(train_file=INPUT_TRAIN_FILE, target ='G3', numeric_features=["age", "G1", "G2"]):
+    df_train=pd.read_csv(train_file)
     print("Training process has begun and it might take a while based on the size of input dataset.")
     #intialize the setup
     exp_reg = setup(df_train, target=target, numeric_features=numeric_features, silent=True)#html=False)
@@ -27,10 +25,12 @@ def train(target = 'G3', numeric_features=["age", "G1", "G2"]):
     save_model(final_model, MODEL_FILE)
     print(f"Saved model file: {MODEL_FILE}")
 
-def pred():
+def pred(test_file=INPUT_TEST_FILE):
+    df_test=pd.read_csv(test_file)
     print(f"Loading model file: {MODEL_FILE}")
     loaded_model = load_model(MODEL_FILE)
     y_pred=loaded_model.predict(df_test)
     df_test[OUTPUT_PRED]=y_pred
-    df_test.to_csv(OUTPUT_PRED_FILE)
+    df_test[OUTPUT_PRED] = df_test[OUTPUT_PRED].round()
+    df_test.to_csv(OUTPUT_PRED_FILE, index=False)
     print(f"Output is written to the file: {OUTPUT_PRED_FILE}")
